b25209d09d0553c768cfe4ebec7f0dc3ee7e0a04
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2021-03-30 14:48:17 -0700

    Create path.cpp

---
+++ PicEdit/path.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class v2 {
+	public:
+		int x;
+		int y;
+};
+
+v2 gv2(int x, int y) {
+	v2 res;
+	res.x = x;
+	res.y = y;
+	return res;
+}
+
+class Path {
+	public:
+		vector<v2> data;
+		string name;
+		char symbol;
+		void addData(v2 a) {
+			data.resize(data.size()+1);
+			data[data.size()-1] = a;
+		}
+
+		void removeData(int idx) {
+			data.erase(data.begin()+idx);
+		}
+};
