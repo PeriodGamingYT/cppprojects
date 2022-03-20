7e3f29ef3fdf07a1c997b596a8b017f3eea0da38
_Arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2022-03-20 14:26:10 -0600

    added stuff to wordstar, works better but complete text editor

--- WordStar/main.cpp
+++ WordStar/main.cpp
@@ -10,14 +10,34 @@ using namespace std;
 
 void clear() {
 	cout << "\x1b[0;0f";
-	for(int i = 0; i <  24; i++) {
-		for(int j = 0; j < 80; j++) cout << " ";
-		cout << "\n";
-	}
+	cout << "\x1b[2J";
 	cout << "\x1b[0;0f";
 }
 
-int main() {
+string showPrompt(string prompt) {
+	clear();
+	cout << prompt;
+	string result = "";
+	getline(cin, result);
+	clear();
+	return result;
+}
+
+void saveFile(string fileName, vector<string> lines) {
+	std::ofstream file(fileName, std::ofstream::trunc);
+	if(!file) {
+		cout << "Error creating file.\n";
+		return;
+	}
+
+	for(string i : lines) {
+		file << i << endl;
+	}
+
+	file.close();
+}
+
+int main(char **argv, int argc) {
 	if(argc == 1) return -1;
 	// Document data.
 	vector<string> lines(1);
@@ -57,7 +77,14 @@ int main() {
 				scol--;
 			}
 		} else if(c == ctrl('s')) {
+			string fileName = showPrompt("Save file with name: ");
+			saveFile(fileName, lines);
 			return 0;
+		} else if(c == ctrl('q')) {
+			string answer = showPrompt("If you want to quit without saving type \"Yes do as I say!!!\": ");
+			if(answer == "Yes do as I say!!!") {
+				return 0;
+			}
 		} else {
 			string s = " ";
 			s[0] = c;
