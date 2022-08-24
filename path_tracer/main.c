eaefe195bfb5d6c243e534dbd59c68830663c659
PeriodGamingYT <nathanryanphillips@gmail.com>
2022-08-24 15:36:00 -0600

    made scene and now making pixmap RGB instead of grayscale

--- path_tracer/main.c
+++ path_tracer/main.c
@@ -19,6 +19,7 @@
 #include <math.h> // For path tracing.
 
 // Section pixmap.
+#define RGB_LENGTH 3
 struct pixmap {
     char *data;
     int width;
@@ -39,7 +40,7 @@ int pixmap_data_index(
         return -1;
     }
     
-    return (arg_y * arg_pixmap->width) + arg_x;
+    return ((arg_y * arg_pixmap->width) + arg_x) * RGB_LENGTH;
 }
 
 int pixmap_area(
@@ -48,7 +49,8 @@ int pixmap_area(
     HANDLE_NULL_RET(arg_pixmap, -1)
     return (
         arg_pixmap->width *
-        arg_pixmap->height  
+        arg_pixmap->height *
+        RGB_LENGTH
     );
 }
 
@@ -62,7 +64,8 @@ void pixmap_setup(
     arg_pixmap->data = (char*)malloc(
         sizeof(char*) *
         arg_pixmap->width *
-        arg_pixmap->height
+        arg_pixmap->height *
+        RGB_LENGTH
     );
 }
 
@@ -78,13 +81,7 @@ void pixmap_output(
         i < pixmap_area(arg_pixmap) - 1;
         i++
     ) {
-        for(
-            int j = 0;
-            j < 3;
-            j++
-        ) {
-            fputc(arg_pixmap->data[i], file);  
-        }
+      fputc(arg_pixmap->data[i], file);  
     }
 
     fclose(file);
@@ -239,22 +236,107 @@ double sphere_intersect(
     : b + detection;
 }
 
-struct vector3 sphere_normal(
-  struct sphere arg_sphere,
-  struct vector3 a
-) {
-  VECTOR3_MATH_ASSIGN(a, arg_sphere.position, -=)
-  double temp = arg_sphere.radius;
-  struct vector3 b = vector3_create(temp, temp, temp);
-  VECTOR3_MATH_ASSIGN(a, b, /=)
-  return a;
-}
+// struct vector3 sphere_normal(
+  // struct sphere arg_sphere,
+  // struct vector3 a
+// ) {
+  // VECTOR3_MATH_ASSIGN(a, arg_sphere.position, -=)
+  // double temp = arg_sphere.radius;
+  // struct vector3 b = vector3_create(temp, temp, temp);
+  // VECTOR3_MATH_ASSIGN(a, b, /=)
+  // return a;
+// }
 
 // Section path tracing.
+inline double clamp(double x) {
+  return
+    x < 0
+    ? 0
+    : x > 1
+      ? 1
+      : x;
+}
+
+inline int to_int(double x) {
+  return (int)(
+    pow(
+      clamp(x),
+      1 / 2.2
+    ) * 255 + .5
+  );
+}
+
+inline int instersect(
+  const struct ray r,
+  double t,
+  int *id,
+  struct sphere *spheres
+) {
+  double n = sizeof(spheres) / sizeof(struct sphere*);
+  double d;
+  double inf = t = 1e20;
+  for(int i = (int)n; i--;) {
+    if(
+      (d = sphere_intersect(spheres[i], r)) &&
+      d < t
+    ) {
+      t = d;
+      *id = i;
+    }
+  }
+
+  return t < inf;
+}
 
 // Section main.
-int main() {
+int main(
+  int argc,
+  char **argv
+) {
+  struct sphere spheres[] = {
+    sphere_create(1e5, vector3_create(1e5 + 1, 40.8, 81.6), vector3_create(0, 0, 0), vector3_create(.75, .25, .25), DIFFUSE),
+    sphere_create(1e5, vector3_create(-1e5 + 99, 40.8, 81.6), vector3_create(0, 0, 0), vector3_create(.25, .25, .75), DIFFUSE),
+    sphere_create(1e5, vector3_create(50, 40.8, 1e5), vector3_create(0, 0, 0), vector3_create(.75, .75, .75), DIFFUSE),
+    sphere_create(1e5, vector3_create(50, 40.8, -1e5 + 170), vector3_create(0, 0, 0), vector3_create(0, 0, 0), DIFFUSE),
+    sphere_create(1e5, vector3_create(50, 1e5, 81.6), vector3_create(0, 0, 0), vector3_create(.75, .75, .75), DIFFUSE),
+    sphere_create(1e5, vector3_create(50, -1e5 + 81.6, 81.6), vector3_create(0, 0, 0), vector3_create(.75, .75, .75), DIFFUSE),
+    sphere_create(1e5, vector3_create(27, 16.5, 47), vector3_create(0, 0, 0), vector3_create(.999, .999, .999), SPECULAR),
+    sphere_create(1e5, vector3_create(73, 16.5, 78), vector3_create(0, 0, 0), vector3_create(.999, .999, .999), REFRACTION),
+    sphere_create(1e5, vector3_create(50, 81.6 - 16.5, 81.6), vector3_create(400, 400, 400), vector3_create(0, 0, 0), DIFFUSE),
+  };
+
+  int spheresLength = sizeof(spheres) / sizeof(struct sphere);
+  int width = 512;
+  int height = 382;
+  int samples = 
+    argc == 2
+    ? atoi(argv[1]) / 4
+    : 1;
+
+  struct ray camera = ray_create(
+    vector3_create(50, 52, 295.6), 
+    vector3_normalize(
+      vector3_create(
+        0,
+        -0.042612,
+        -1
+      )
+    )
+  );
+
+  double temp = width * .5135 / height;
+  struct vector3 camera_x = vector3_create(temp, temp, temp);
+  struct vector3 camera_y = vector3_normalize(
+    vector3_cross(
+      camera_x, 
+      camera.direction
+    )
+  );
+
+  VECTOR3_MATH_ASSIGN(camera_y, vector3_create(.5135, .5135, .5135), *)
+  struct vector3 r;
   struct pixmap current_pixmap;
+  // Then create image.
   pixmap_setup(WIDTH_AND_HEIGHT, &current_pixmap);
   pixmap_output("test.ppm", &current_pixmap);
   return 0;
